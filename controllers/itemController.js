407e555b6f65c6aefc6b31bdbcee097cd634558b
Milan B <milan.blasko@interway.sk>
2025-02-05 14:00:28 +0100

    initial commit

---
+++ controllers/itemController.js
@@ -0,0 +1,17 @@
+const Item = require("../models/itemModel");
+
+exports.getItems = async (req, res) => {
+  const items = await Item.find();
+  res.json(items);
+};
+
+exports.createItem = async (req, res) => {
+  const newItem = new Item(req.body);
+  await newItem.save();
+  res.status(201).json(newItem);
+};
+
+exports.deleteItem = async (req, res) => {
+  await Item.findByIdAndDelete(req.params.id);
+  res.json({ message: "Item deleted successfully" });
+};
